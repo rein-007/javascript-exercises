1febec2510b06ceb2a2002d8910e601afc634a36
rein-007 <182231134+rein-007@users.noreply.github.com>
2024-10-05 17:44:08 +0800

    done until 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let num = word.length;
+    let string = '';
+    for (let i=num-1;i>=0;i--) {
+        string+=word[i];
+    }
+    return string;
 };
 
 // Do not edit below this line
