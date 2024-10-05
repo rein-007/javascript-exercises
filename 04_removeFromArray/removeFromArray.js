0a810a04cf5f1d7a2691972453d60ca1d24676b2
rein-007 <182231134+rein-007@users.noreply.github.com>
2024-10-05 19:04:03 +0800

    done 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(theArray,...theArgs) {
+    let result = [];
+
+    for (let i = 0;i<theArray.length;i++) {
+        // for (let x = 0; x<theArgs.length; x++) {
+            if (!theArgs.includes(theArray[i])) {
+            // if (theArray[i] != theArgs[x]) {
+                result = result.concat(theArray[i]);
+            }
+        // }
+    }
+    return result;
 };
 
 // Do not edit below this line
