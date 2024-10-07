75a3b41b8ae8f462a91565f64785ad4d7325351c
rein-007 <182231134+rein-007@users.noreply.github.com>
2024-10-07 15:29:59 +0800

    done until 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let result = 0;
+    let i = 0;
+    if (typeof(firstNum+secondNum) === 'number' && Number.isInteger(firstNum+secondNum) === true && firstNum>=0 && secondNum>=0) {
+        if (firstNum>secondNum) {
+            const num = firstNum;
+            firstNum = secondNum;
+            secondNum = num;
+        }
+        
+        for (i = firstNum;i<=secondNum;i++) {
+            result += i;
+        }
+    } else {
+        result = 'ERROR';
+    }
 
+    return result;
 };
 
 // Do not edit below this line
