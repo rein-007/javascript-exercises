87213a6bb9ba20bcdf9ed6183b58c0611f0ea6fd
rein-007 <182231134+rein-007@users.noreply.github.com>
2024-10-09 19:02:46 +0800

    done 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arg) {
+  result = arg.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
+  if (arg.length==0) {
+    result = 0;
+  } else if (arg.length==1) {
+    result = arg[0];
+  }
+	return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arg) {
+  result = arg.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(arg) {
+  let result = 1;
+
+  if (arg==0 || arg==1) {
+    return result;
+  }
+
+  for (let i = 2; i <= arg; i++) {
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
